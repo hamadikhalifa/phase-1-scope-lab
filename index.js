3df1a35282519eed5f1eb92ce2750ec3e3ba740f
hamadikhalifa <ekidorevans719@gmail.com>
2025-03-09 01:16:36 +0300

    implement functions for modifying global variables and testing scope behavior

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'; // Declaring a global variable inside a function (bad practice)
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'; // Reassigning the global bestCustomer
+}
+
+const leastFavoriteCustomer = 'someone';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'someone else'; // This will throw an error since const cannot be reassigned
+}
